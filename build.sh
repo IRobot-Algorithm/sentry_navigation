23329458d4c7cbe7bcdfbd073b3a161df871fdcd
niuoruo <1102567801@qq.com>
2024-07-11 06:00:49 +0800

    fix : 修复编译顺序bug

--- build.sh
+++ build.sh
@@ -8,5 +8,5 @@ source /opt/ros/noetic/setup.bash
 catkin_make -DCATKIN_WHITELIST_PACKAGES="livox_ros_driver2"
 catkin_make -DCATKIN_WHITELIST_PACKAGES="sentry_srvs"
 catkin_make -DCATKIN_WHITELIST_PACKAGES="sentry_msgs"
-catkin_make -DCATKIN_WHITELIST_PACKAGES=""
+catkin_make -j1 -DCATKIN_WHITELIST_PACKAGES="fast_lio"
 catkin_make -DCATKIN_WHITELIST_PACKAGES=""
