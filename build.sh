bab39e9ef253fc367828b7f340463027c36f43d1
niuoruo <1102567801@qq.com>
2024-07-10 20:02:25 +0800

    feat : Add docker

--- build.sh
+++ build.sh
@@ -1,9 +1,12 @@
-#bin/bash
+#! /bin/bash
+set +e
 
 source /opt/ros/noetic/setup.bash
 ./src/livox_ros_driver2/build.sh ROS1
 
+source /opt/ros/noetic/setup.bash
 catkin_make -DCATKIN_WHITELIST_PACKAGES="livox_ros_driver2"
 catkin_make -DCATKIN_WHITELIST_PACKAGES="sentry_srvs"
 catkin_make -DCATKIN_WHITELIST_PACKAGES="sentry_msgs"
+catkin_make -DCATKIN_WHITELIST_PACKAGES=""
 catkin_make -DCATKIN_WHITELIST_PACKAGES=""
