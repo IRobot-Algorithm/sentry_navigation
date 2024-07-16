bce3be4b53e6bafa418b891b17e37cc3ab110d41
niuoruo <1102567801@qq.com>
2024-07-16 21:51:19 +0800

    style : 删除debug话题

--- src/nav_transporter/src/state_processing.cpp
+++ src/nav_transporter/src/state_processing.cpp
@@ -389,7 +389,7 @@ void StateProcess::loop(const ros::TimerEvent& event)
         pub_map_reset_.publish(msg);
       }
     }
-    publishPolygons();
+    // publishPolygons();
     nav_num = 0;
   }
 
