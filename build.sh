0575472d9ef31863c8279432a3640e62d5a38362
niuoruo <1102567801@qq.com>
2024-08-28 21:09:59 +0800

    docs : 修改build.sh

--- build.sh
+++ build.sh
@@ -9,7 +9,8 @@ catkin_make -DCATKIN_WHITELIST_PACKAGES="livox_ros_driver2"
 source /opt/ros/noetic/setup.bash
 catkin_make -DCATKIN_WHITELIST_PACKAGES="sentry_srvs"
 catkin_make -DCATKIN_WHITELIST_PACKAGES="sentry_msgs"
-catkin_make -j1 -DCATKIN_WHITELIST_PACKAGES="fast_lio"
+catkin_make -DCATKIN_WHITELIST_PACKAGES="fast_lio"
+catkin_make -DCATKIN_WHITELIST_PACKAGES="faster_lio"
+catkin_make -DCATKIN_WHITELIST_PACKAGES="point_lio"
 source /opt/ros/noetic/setup.bash
-catkin_make -DCATKIN_WHITELIST_PACKAGES=""
 catkin_make -DCATKIN_WHITELIST_PACKAGES=""
